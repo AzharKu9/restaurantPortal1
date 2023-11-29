61e874254bc9978e2f9696178a7685041cd7ad9a
Hamza <waleedghori4@gmail.com>
2023-11-29 21:00:33 +0500

    view design change

--- src/Pages/ViewProduct.js
+++ src/Pages/ViewProduct.js
@@ -4,11 +4,98 @@ import ModalUpdate from './ModalUpdate';
 const ViewProduct = () => {
   const [modelOpen , setModalOpen] = useState(false)
   return (
-    <div className='container max-w-5xl bg-white'>
+    <div className='container m-h-full max-w-full pb-20 bg-white'>
       <h2 className='text-4xl mt-2 mb-4'>View Food</h2>
       <hr className='mb-6'></hr>
-      <div className=''>
-        <div className='h-[30rem] overflow-auto custom-scrollbar'>
+      <div className='flex flex-wrap  justify-center h-auto'>
+        <div className='h-1/3 w-[30%] m-4 rounded-md shadow-lg shadow-gray-400' >
+          <img src='./image.avif' alt='iamge'/>
+            <h2 className='px-3 py-1 mt-4 text-lg font-sans font-semibold'><strong>Food Title:</strong> Biryani with salad</h2>
+            <p className='px-3 py-1 text-lg font-sans font-semibold'> <strong>Food Description:</strong> Amazing jusicy flavour with amazing gernish and imported ingridents...</p>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Price:</strong> 130 Rs.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Quantity:</strong> 1kg.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Offer</strong> 30%of</h2>
+            <div className='my-2 mx-2'>
+            <button onClick={()=>setModalOpen(true)} className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Update</button>
+            <button className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Delete</button>
+            </div>
+        </div>
+        <div className='h-1/3 w-[30%] m-4 rounded-md shadow-lg shadow-slate-400' >
+          <img src='./image.avif' alt='iamge'/>
+            <h2 className='px-3 py-1 mt-4 text-lg font-sans font-semibold'><strong>Food Title:</strong> Biryani with salad</h2>
+            <p className='px-3 py-1 text-lg font-sans font-semibold'> <strong>Food Description:</strong> Amazing jusicy flavour with amazing gernish and imported ingridents...</p>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Price:</strong> 130 Rs.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Quantity:</strong> 1kg.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Offer</strong> 30%of</h2>
+            <div className='my-2 mx-2'>
+            <button onClick={()=>setModalOpen(true)} className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Update</button>
+            <button className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Delete</button>
+            </div>
+        </div>
+        <div className='h-1/3 w-[30%] m-4 rounded-md shadow-lg shadow-slate-400' >
+          <img src='./image.avif' alt='iamge'/>
+            <h2 className='px-3 py-1 mt-4 text-lg font-sans font-semibold'><strong>Food Title:</strong> Biryani with salad</h2>
+            <p className='px-3 py-1 text-lg font-sans font-semibold'> <strong>Food Description:</strong> Amazing jusicy flavour with amazing gernish and imported ingridents...</p>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Price:</strong> 130 Rs.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Quantity:</strong> 1kg.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Offer</strong> 30%of</h2>
+            <div className='my-2 mx-2'>
+            <button onClick={()=>setModalOpen(true)} className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Update</button>
+            <button className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Delete</button>
+            </div>
+        </div>
+        <div className='h-1/3 w-[30%] m-4 rounded-md shadow-lg shadow-slate-400' >
+          <img src='./image.avif' alt='iamge'/>
+            <h2 className='px-3 py-1 mt-4 text-lg font-sans font-semibold'><strong>Food Title:</strong> Biryani with salad</h2>
+            <p className='px-3 py-1 text-lg font-sans font-semibold'> <strong>Food Description:</strong> Amazing jusicy flavour with amazing gernish and imported ingridents...</p>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Price:</strong> 130 Rs.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Quantity:</strong> 1kg.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Offer</strong> 30%of</h2>
+            <div className='my-2 mx-2'>
+            <button onClick={()=>setModalOpen(true)} className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Update</button>
+            <button className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Delete</button>
+            </div>
+        </div>
+        <div className='h-1/3 w-[30%] m-4 rounded-md shadow-lg shadow-slate-400' >
+          <img src='./image.avif' alt='iamge'/>
+            <h2 className='px-3 py-1 mt-4 text-lg font-sans font-semibold'><strong>Food Title:</strong> Biryani with salad</h2>
+            <p className='px-3 py-1 text-lg font-sans font-semibold'> <strong>Food Description:</strong> Amazing jusicy flavour with amazing gernish and imported ingridents...</p>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Price:</strong> 130 Rs.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Quantity:</strong> 1kg.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Offer</strong> 30%of</h2>
+            <div className='my-2 mx-2'>
+            <button onClick={()=>setModalOpen(true)} className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Update</button>
+            <button className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Delete</button>
+            </div>
+        </div>
+        <div className='h-1/3 w-[30%] m-4 rounded-md shadow-lg shadow-slate-400' >
+          <img src='./image.avif' alt='iamge'/>
+            <h2 className='px-3 py-1 mt-4 text-lg font-sans font-semibold'><strong>Food Title:</strong> Biryani with salad</h2>
+            <p className='px-3 py-1 text-lg font-sans font-semibold'> <strong>Food Description:</strong> Amazing jusicy flavour with amazing gernish and imported ingridents...</p>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Price:</strong> 130 Rs.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Quantity:</strong> 1kg.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Offer</strong> 30%of</h2>
+            <div className='my-2 mx-2'>
+            <button onClick={()=>setModalOpen(true)} className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Update</button>
+            <button className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Delete</button>
+            </div>
+        </div>
+        <div className='h-1/3 w-[30%] m-4 rounded-md shadow-lg shadow-slate-400' >
+          <img src='./image.avif' alt='iamge'/>
+            <h2 className='px-3 py-1 mt-4 text-lg font-sans font-semibold'><strong>Food Title:</strong> Biryani with salad</h2>
+            <p className='px-3 py-1 text-lg font-sans font-semibold'> <strong>Food Description:</strong> Amazing jusicy flavour with amazing gernish and imported ingridents...</p>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Price:</strong> 130 Rs.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Quantity:</strong> 1kg.</h2>
+            <h2 className='px-3 py-1 text-lg font-sans font-semibold'><strong>Food Offer</strong> 30%of</h2>
+            <div className='my-2 mx-2'>
+            <button onClick={()=>setModalOpen(true)} className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Update</button>
+            <button className='px-4 py-2 m-2 text-lg font-medium bg-[#FEC013] hover:bg-[#ffd04f] rounded'>Delete</button>
+            </div>
+        </div>
+    
+        
+
+        {/* <div className='h-[30rem] overflow-auto custom-scrollbar'>
           <table className='w-full border-collapse'>
             <thead>
               <tr>
@@ -52,8 +139,9 @@ const ViewProduct = () => {
   
             </tbody>
           </table>
-        </div>
+        </div> */}
       </div>
+
 
       {modelOpen && <ModalUpdate setModalOpen={setModalOpen} />}
     </div>
