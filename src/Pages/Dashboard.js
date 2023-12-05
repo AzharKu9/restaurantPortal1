46b9478c505fce83b7172dfdea10689b7f807bdf
Hamza <waleedghori4@gmail.com>
2023-12-05 18:48:57 +0500

    gteorder

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import AddProduct from './AddProduct';
 import ViewProduct from './ViewProduct';
 import GetOrder from './GetOrder';
 import Home from './Home';
 import Setting from './Setting';
 import { Navigate} from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import { toast } from 'react-toastify';
 
 const Dashboard = () => {
-
+  const {logout} = useContext(AuthContext)
   const [selectedTab, setSelectedTab] = useState('home'); // State to manage selected tab
 
   const changeTab = (tabName) => {
     setSelectedTab(tabName);
   };
   const handleLogout = ()=>{
-      localStorage.removeItem("authToken")
-      return <Navigate to="/login" />;
-    
+    toast.success("Logout!")
+    logout()    
   }
 
 
@@ -27,47 +28,47 @@ const Dashboard = () => {
         <div className="p-4 mt-2 border-b" >
           <h1 className="text-lg font-bold">Dashboard</h1>
         </div>
-        <div className="p-4">
+        <div className="py-4">
           {/* Navigation */}
           <ul>
             <li
               onClick={() => changeTab('home')}
-              className={`cursor-pointer py-2 ${selectedTab === 'home' ? 'text-black ml-[2px] pl-2 font-bold bg-[#fbb800] rounded-md' : ''
+              className={`cursor-pointer py-2 pl-4 border-b-2 border-[#fbb800] ${selectedTab === 'home' ? 'text-black font-bold bg-[#fbb800] rounded-sm' : ''
                 }`}
             >
               Home
             </li>
             <li
               onClick={() => changeTab('addProduct')}
-              className={`cursor-pointer py-2 ${selectedTab === 'addProduct' ? 'text-black ml-[2px] pl-2 font-bold bg-[#fbb800] rounded-md' : ''
+              className={`cursor-pointer py-2 pl-4 border-b-2 border-[#fbb800] ${selectedTab === 'addProduct' ? 'text-black  font-bold bg-[#fbb800] rounded-sm' : ''
                 }`}
             >
               Add Dishes
             </li>
             <li
               onClick={() => changeTab('viewDishes')}
-              className={`cursor-pointer py-2 ${selectedTab === 'viewDishes' ? 'text-black ml-[2px] pl-2 font-bold bg-[#fbb800] rounded-md' : ''
+              className={`cursor-pointer py-2 pl-4 border-b-2 border-[#fbb800] ${selectedTab === 'viewDishes' ? 'text-black  font-bold bg-[#fbb800] rounded-sm' : ''
                 }`}
             >
               View Dishes
             </li>
             <li
               onClick={() => changeTab('getOrder')}
-              className={`cursor-pointer py-2 ${selectedTab === 'getOrder' ? 'text-black ml-[2px] pl-2 font-bold bg-[#fbb800] rounded-md' : ''
+              className={`cursor-pointer py-2 pl-4 border-b-2 border-[#fbb800] ${selectedTab === 'getOrder' ? 'text-black font-bold bg-[#fbb800] rounded-sm' : ''
                 }`}
             >
               Get Order
             </li>
             <li
               onClick={() => changeTab('setting')}
-              className={`cursor-pointer py-2 ${selectedTab === 'setting' ? 'text-black ml-[2px] pl-2 font-bold bg-[#fbb800] rounded-md' : ''
+              className={`cursor-pointer py-2 pl-4 border-b-2 border-[#fbb800] ${selectedTab === 'setting' ? 'text-black font-bold bg-[#fbb800] rounded-sm' : ''
                 }`}
             >
               Setting
             </li>
             {/* Add more navigation items as needed */}
           </ul>
-        <div className='absolute left-0 bg-[#FEC013] bottom-32 py-2 w-full rounded-sm flex justify-center'>
+        <div className='absolute left-0  bg-[#FEC013] bottom-32 py-2 w-full rounded-sm flex justify-center'>
           <button onClick={handleLogout} className='font-semibold'>Logout</button>
         </div>
         </div>
--- src/Pages/GetOrder.js
+++ src/Pages/GetOrder.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
 
 const GetOrder = () => {
+
   return (
-    <div>GetOrder</div>
+    <div className='container  min-h-screen max-h-full'>
+      <h2 className='text-4xl mt-2 mb-4'>Get Order</h2>
+      <hr className='mb-6'></hr>
+      <table className="w-full border-collapse border">
+  <thead>
+    <tr className="bg-yellow-300">
+      <th className="p-2 border">Order No</th>
+      <th className="p-2 border">Food Title</th>
+      <th className="p-2 border">Food Quantity</th>
+      <th className="p-2 border">Food Price</th>
+      <th className="p-2 border">Status</th>
+      <th className="p-2 border">Change Status</th>
+      <th className="p-2 border">Received Time</th>
+      <th className="p-2 border"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr className="bg-gray-50">
+      <td className="p-2 border">1</td>
+      <td className="p-2 border">Burger</td>
+      <td className="p-2 border">2</td>
+      <td className="p-2 border">$10.00</td>
+      <td className="p-2 border">Pending</td>
+      <td className="p-2 border">
+        <select className="p-2">
+          <option>Completed</option>
+          <option>Rejected</option>
+        </select>
+      </td>
+      <td className="p-4 border"></td>
+    </tr>
+    {/* Add more rows as needed */}
+  </tbody>
+</table>
+
+    </div>
   )
 }
 
